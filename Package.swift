b6eb77a33e9903c699ecc558305fc27ef5de7f9e
Swizzlr <git@swizzlr.co>
2017-01-12 13:54:31 +0000

    Update dep URLs.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 var package = Package(
   name: "TitanKituraAdapter",
   dependencies: [
-    .Package(url: "https://github.com/bermudadigitalstudio/titan-core.git", majorVersion: 0, minor: 1),
+    .Package(url: "https://github.com/bermudadigitalstudio/TitanCore.git", majorVersion: 0, minor: 1),
     .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 4)
   ]
 )
