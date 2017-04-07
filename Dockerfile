a98f930f950e2da6c84ee8266ad7824a97d49ef9
Laurent Gaches <laurent@binimo.com>
2017-04-07 13:11:14 +0200

    update Kitura-net dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
-FROM swiftdocker/swift:3.0.2
+FROM swift:3.1
 
-RUN apt-get update && \
-    apt-get install -y openssl libssl-dev &&\
-    apt-get clean && \
-    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 COPY Package.swift /code/Package.swift
 WORKDIR /code
 RUN swift package fetch
--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ var package = Package(
   name: "TitanKituraAdapter",
   dependencies: [
     .Package(url: "https://github.com/bermudadigitalstudio/TitanCore.git", majorVersion: 0, minor: 3),
-    .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 4)
+    .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 7)
   ]
 )
